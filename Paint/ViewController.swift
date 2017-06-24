7e195860cb0f6cbb6437a66b61fad0328fd1f46d
Marwa Kharbech <marwa.kharbeche@gmail.com>
2017-06-24 14:17:56 -0400

    Initial Commit

---
+++ Paint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Paint
+//
+//  Created by My Computer on 2017-06-24.
+//  Copyright © 2017 Marwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
